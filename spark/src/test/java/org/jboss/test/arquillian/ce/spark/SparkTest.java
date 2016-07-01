c23c99547ce72ef32d2abde4a8017ea541223a8c
root <root@mrg44.lab.bos.redhat.com>
2016-06-30 21:55:45 -0400

    simplfied containerish tests

--- spark/src/test/java/org/jboss/test/arquillian/ce/spark/SparkTest.java
+++ spark/src/test/java/org/jboss/test/arquillian/ce/spark/SparkTest.java
@@ -73,8 +73,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 @Template(
-        url="https://gist.githubusercontent.com/jayunit100/3fad23e42a22e473fff7a45947bd3102/raw/ccedbb3020d98267753ba758c9f739d587b3aae5/xpass-spark",
-        labels = "app=spark",
+	
+	// url = "https://gist.githubusercontent.com/jayunit100/c2e29f30432d346bb8a9fddb6e8d2012/raw/2b042b275f1e5f5a21014c2b81849cf95fb297dd/xpass-spark.yaml",
+	// with ui disabled...
+	url = "https://gist.githubusercontent.com/jayunit100/c2e29f30432d346bb8a9fddb6e8d2012/raw/35952fad20ac34b8ee8a1aac80c81ba6c91b2eb7/xpass-spark.yaml",
+	labels = "app=spark",
         parameters = {
                 @TemplateParameter(name = "MASTER_NAME", value="jspark")
 		// use this if running outside the VPN.
@@ -99,57 +102,70 @@ public class SparkTest {
     public static WebArchive getDeployment() {
         WebArchive war = ShrinkWrap.create(WebArchive.class, "run-in-pod.war");
         war.setWebXML(new StringAsset("<web-app><context-param><param-name>resteasy.scan.providers</param-name><param-value>false</param-value></context-param></web-app>"));
-        war.addPackage(org.jboss.arquillian.test.impl.EventTestRunnerAdaptor.class.getPackage());
-        war.addPackage(Arquillian.class.getPackage());
-        war.addPackage(org.apache.spark.internal.Logging.class.getPackage());
-        war.addPackage(org.apache.spark.api.java.function.Function.class.getPackage());
-        war.addPackage(org.apache.spark.SparkConf.class.getPackage());
-        war.addPackage(scala.Cloneable.class.getPackage());
-        war.addPackage(SparkTest.class.getPackage());
-
-        //catch all because im bored of finding new ones...
-        war.addPackages(true,"org.apache.*");
-        war.addPackages(true,"org.apache.spark");
-        war.addPackages(true,"org.apache.commons");
-        war.addPackages(true,"org.apache.hadoop");
-        war.addPackages(true,"scala");
-        war.addPackages(true,"org.slf4j");
-        war.addPackages(true,"org.spark_project.guava");
-        war.addPackages(true,"com.google.common");
-        war.addPackages(true,"io.netty");
-        war.addPackages(true,"com.esotericsoftware");
-        war.addPackages(true,"com.twitter");
-        war.addPackages(true,"com.codahale");
-        war.addPackages(true,"org.json4s");
-	war.addPackages(true,"org.spark_project.jetty");
-war.addPackages(true,"org.apache.spark.static");
-
-	// no op
-	// war.addPackages(true,"org.apache.spark.ui");
-	// war.addAsResource(Package.getPackage("org.apache.spark"),"/");
-	// war.addAsResource(org.apache.spark.SparkConf.class.getPackage(),"org/apache/spark");
+        
 	
-	// NullPointerException
-	// war.addAsResource(org.apache.spark.ui.WebUI.class.getPackage(),"static");
+	war.addPackage(org.jboss.arquillian.test.impl.EventTestRunnerAdaptor.class.getPackage());
+        war.addPackage(Arquillian.class.getPackage());
+        
 
-	war.addAsResource(org.apache.spark.ui.WebUI.class.getPackage(),"static/additional-metrics.js");
-	// leads to resteasy conflict
-	// war.addPackages(true, "org.glassfish.jersey.server");
+	/**    Uncomment this if attempting to run spark arquillian unit test w/ JavaSparkContext.
+	       these deps were carefully pruned, and are mostly what are required to get things working.
 
+		war.addPackage(org.apache.spark.internal.Logging.class.getPackage());
+		war.addPackage(org.apache.spark.api.java.function.Function.class.getPackage());
+		war.addPackage(org.apache.spark.SparkConf.class.getPackage());
+		war.addPackage(scala.Cloneable.class.getPackage());
+		war.addPackage(SparkTest.class.getPackage());
+
+		//catch all because im bored of finding new ones...
+		war.addPackages(true,"org.apache.*");
+		war.addPackages(true,"org.apache.spark");
+		war.addPackages(true,"org.apache.commons");
+		war.addPackages(true,"org.apache.hadoop");
+		war.addPackages(true,"scala");
+		war.addPackages(true,"org.slf4j");
+		war.addPackages(true,"org.spark_project.guava");
+		war.addPackages(true,"com.google.common");
+		war.addPackages(true,"io.netty");
+		war.addPackages(true,"com.esotericsoftware");
+		war.addPackages(true,"com.twitter");
+		war.addPackages(true,"com.codahale");
+		war.addPackages(true,"org.json4s");
+		war.addPackages(true,"org.spark_project.jetty");
+	war.addPackages(true,"org.apache.spark.static");
+
+		war.addAsResource(org.apache.spark.ui.WebUI.class.getPackage(),"static/additional-metrics.js");
+		
+		// leads to resteasy conflict: alternative, disable spark webui.
+		// war.addPackages(true, "org.glassfish.jersey.server");
+	
+	**/ 
 	return war;
     }
-
+ 
     @Test
     @InSequence(1)
-    public void testSparkWebUI() throws Exception {
-        /**
+    public void testSmoke() throws Exception {
+	// In addition to the containers spinning up, asser that the web ui spins up.
+	URL u = new URL("http://jspark-webui:8080");
+	HttpURLConnection huc =  (HttpURLConnection)  u.openConnection();
+ 	assertEquals(WEB_UI_EXPECT,huc.getResponseCode());
+    }
+
+    /**
+    *  This test doesnt completely work.
+    *  However, it represents a "customer like" workflow for jboss, in that it natively calls spark from java.
+    *  So, we should refine it over time and get it working (requires jboss dependency wrangling).
+     
+    @org.junit.Ignore
+    @Test
+    public void testSparkInJBossContainer() throws Exception {
 
          // TODO put this in a separate test.
          URL u = new URL("http://jspark-webui:8080");
          HttpURLConnection huc =  (HttpURLConnection)  u.openConnection();
          System.out.println(huc.getResponseCode());
          assertEquals(WEB_UI_EXPECT,huc.getResponseCode());
-         **/
 
         // Setup nss stuffs. otherwise spark wont have  a user name and will throw a null exception .
         String[] cmd = new String[]{"/bin/bash",
@@ -168,21 +184,39 @@ war.addPackages(true,"org.apache.spark.static");
 
 	conf.set("spark.ui.enabled","false");
 
-        JavaSparkContext sc = new JavaSparkContext(conf);
-	
-        List<String> uniqueHosts = sc.parallelize(new ArrayList<Integer>(100)).map(
-                new Function<Integer, String>() {
-                    @Override
-                    public String call(Integer v1) throws Exception {
-                        return InetAddress.getLocalHost().getHostName();
-                    }
-                }).distinct().collect();
-        for (String s : uniqueHosts){
-            System.out.println("HOST !!! "+s);
-            Thread.sleep(1);
-        }
+	JavaSparkContext sc = null;
 
+	try { 
+        	sc = new JavaSparkContext(conf);
+	} catch (Throwable t) {
+		throw new Exception("Got here, creating context, threw "+t.getMessage());
+	}	
+
+	List<String> uniqueHosts = null;
+	try{
+        		uniqueHosts = sc.parallelize(new ArrayList<Integer>(100)).map(
+                	new Function<Integer, String>() {
+             	       		@Override
+             	       		public String call(Integer v1) throws Exception {
+                	        	return "hostname is ......"; 
+					// return InetAddress.getLocalHost().getHostName();
+                		}
+                	}).distinct().collect();
+			try { 
+				for (String s : uniqueHosts){
+				    System.out.println("HOST !!! "+s);
+		    		    Thread.sleep(1);
+				}
+			}
+ 			catch(Throwable t){
+				throw new Exception("dies while printing hosts and sleeping!");
+			}
+       } 
+	catch(Throwable t){
+		throw new Exception("dies due to a weird exception : "+t);
+	}
         // Assert at least 2 unique hosts collected.
         Assert.assertTrue("Asserting that unique hosts (" +uniqueHosts +") size "+uniqueHosts.size() +" Is greater than 2",uniqueHosts.size() > 2);
     }
+	*/
 }
